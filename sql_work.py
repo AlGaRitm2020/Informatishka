57115ea102acfebeaeeb92bbe022aa5ab780e800
joyin1211 <54205695+joyin1211@users.noreply.github.com>
2021-04-14 17:23:36 +0400

    Add files via upload

--- sql_work.py
+++ sql_work.py
@@ -1,8 +1,17 @@
 import sqlite3
 
 
-def register():
-    pass
+def register(username, chat_id):
+    con = sqlite3.connect("users.sqlite")
+    cur = con.cursor()
+    request = "SELECT * FROM users\
+WHERE chat_id={}".format(str(chat_id))
+    usrs_with_id = cur.execute(request).fetchall()
+    if len(usrs_with_id):
+        return False
+    request = "INSERT INTO users(username, chat_id)\
+'\n'VALUES({}, {})".format(username, str(chat_id))
+    return True
 
 
 def add_score():
