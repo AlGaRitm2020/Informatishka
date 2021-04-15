faf9765f11a19e61036887936df5c074eff49d9c
User1 <User1@WIN-7JKQ31MUP7B>
2021-04-15 20:40:09 +0400

    ok done

---
+++ sql_work.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+
+def register(username, chat_id):
+    con = sqlite3.connect("users.sqlite")
+    cur = con.cursor()
+    request = "SELECT * FROM users\
+WHERE chat_id={}".format(str(chat_id))
+    usrs_with_id = cur.execute(request).fetchall()
+    if len(usrs_with_id):
+        return False
+    request = "INSERT INTO users(username, chat_id)\
+'\n'VALUES({}, {})".format(username, str(chat_id))
+    return True
+
+
+def add_score():
+    pass
+
+
+def get_stats():
+    pass
