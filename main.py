f60cc6a6251763c3caabb7639cf98e86ccc8a5bc
User1 <User1@WIN-7JKQ31MUP7B>
2021-04-16 20:31:05 +0400

    inserts doesn't work, idk why

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import parser_shit
 import bullshit
-
+import sql_work
 from telegram import Update, ForceReply, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
 from config import TOKEN
@@ -11,6 +11,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 ANSWER = ""
+TASKNUM = 0
 logger = logging.getLogger(__name__)
 
 
@@ -19,6 +20,14 @@ def start(update: Update, context: CallbackContext):
 Выбери номер задания, я выдам тебе задачу. Введи ответ и я проверю его правильность. \
 Введите команду /practice, чтобы начать решать задания. \
 Чтобы смотреть теорию, напишите /theory')
+
+
+def register(update: Update, context: CallbackContext):
+    result = sql_work.register(update.message.from_user.name, update.message.chat_id)
+    if not result:
+        update.message.reply_text("Вы уже зарегистрированы")
+    else:
+        update.message.reply_text("Вы успешно зарегистрировались.")
 
 
 def conv_begin(update: Update, context: CallbackContext):
@@ -42,6 +51,7 @@ def practice(update: Update, context: CallbackContext):
         if int(nom) < 1 or int(nom) > 27:
             update.message.reply_text("Номер задания от 1 до 27, попробуй еще раз")
             return 1
+        TASKNUM = nom
         info = parser_shit.getTaskByNum(nom)
         answer = info[1]
         task = info[0]
@@ -81,6 +91,14 @@ def practice(update: Update, context: CallbackContext):
         return 1
 
 
+def stats(update, context):
+    result = sql_work.get_stats(update.message.chat_id)
+    if not result:
+        update.message.reply_text("Вы не можете смотреть свою статистику, не зарегистрировавшись")
+        return
+    for item in result:
+        update.message.reply_text("На задаче {} у вас {} успешных решений за последнюю неделю.".format(item, result[item]))
+
 def theory(update, context):
     if update.message.text == '/stop':
         update.message.reply_text('Привет, я бот Информатишка. Я помогу тебе в сдаче ЕГЭ по информатике. \
@@ -90,32 +108,32 @@ def theory(update, context):
         return ConversationHandler.END
     try:
         themes_list = ['https://code-enjoy.ru/ege_po_informatike_2021_zadanie_1_osobie_tochki/',
-                   'https://code-enjoy.ru/ege_po_informatike_zadanie_2_moshneyshiy_metod/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_3_baza_dannih/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_4_uslovie_fano/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_5_algoritmi_avtomati/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_6_cikli/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_7_foto_zvuk/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_8_super_razbor/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_9_tablica_excel/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_10_tekstoviy_redaktor/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_11_kolichestvo_informacii/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_12_ukrosheniye_robota/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_13_legkoe/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_14_chempionskaya_podgotoka/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_15_prostim_yazikom/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_16_rekursiya/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_17_pishem_programmu/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_18_tablica_chisel/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_19_igraem_i_viigrivaem/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_20_ubileyniy_vipusk/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_21_igroki_v_igre/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_22_analiziruem_programmu/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_23_opiraemsa_na_predidushie_shagi/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_24_obrabotka_simvolnoy_informacii/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_25_obrabotka_celochislennoy_informacii/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_26_sortirovka/',
-                   'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_27_zakluchitelnoye/']
+                       'https://code-enjoy.ru/ege_po_informatike_zadanie_2_moshneyshiy_metod/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_3_baza_dannih/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_4_uslovie_fano/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_5_algoritmi_avtomati/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_6_cikli/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_7_foto_zvuk/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_8_super_razbor/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_9_tablica_excel/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_10_tekstoviy_redaktor/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_11_kolichestvo_informacii/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_12_ukrosheniye_robota/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_13_legkoe/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_14_chempionskaya_podgotoka/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_15_prostim_yazikom/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_16_rekursiya/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_17_pishem_programmu/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_18_tablica_chisel/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_19_igraem_i_viigrivaem/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_20_ubileyniy_vipusk/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_21_igroki_v_igre/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_22_analiziruem_programmu/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_23_opiraemsa_na_predidushie_shagi/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_24_obrabotka_simvolnoy_informacii/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_25_obrabotka_celochislennoy_informacii/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_26_sortirovka/',
+                       'https://code-enjoy.ru/ege_po_informatike_2021_zadanie_27_zakluchitelnoye/']
         id = int(update.message.text)
         update.message.reply_text('По этой теме можешь почитать теорию по ссылке:\
 {}'.format(themes_list[id - 1]))
@@ -127,6 +145,7 @@ def theory(update, context):
 
 
 def check(update: Update, context: CallbackContext):
+    global TASKNUM
     if update.message.text == '/stop':
         update.message.reply_text('Привет, я бот Информатишка. Я помогу тебе в сдаче ЕГЭ по информатике. \
     Выбери номер задания, я выдам тебе задачу. Введи ответ и я проверю его правильность. \
@@ -141,10 +160,17 @@ def check(update: Update, context: CallbackContext):
     user_answer.rstrip()
     if str(ANSWER) == str(user_answer):
         update.message.reply_text('Вы аблолютно правы. Ответ: {}'.format(user_answer))
+        global TASKNUM
+        status = sql_work.add_score(TASKNUM, 1, update.message.chat_id)
+        if not status:
+            update.message.reply_text("Вы еще не зарегистрированы, поэтому эт решение не учитывается в статистике.")
         return ConversationHandler.END
     else:
         update.message.reply_text('Ваш ответ неверен. Ответ: {}. \
 Чтобы решать дальше напшите /practice'.format(ANSWER))
+        status = sql_work.add_score(TASKNUM, 0, update.message.chat_id)
+        if not status:
+            update.message.reply_text("Вы еще не зарегистрированы, поэтому эт решение не учитывается в статистике.")
         return ConversationHandler.END
 
 
@@ -162,6 +188,8 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("reg", register))
+    dispatcher.add_handler(CommandHandler("stats", stats))
     dispatcher.add_handler(CommandHandler("send", send_photo))
     dispatcher.add_handler(CommandHandler("start", start))
     Dialog = ConversationHandler(
--- sql_work.py
+++ sql_work.py
@@ -5,19 +5,61 @@ def register(username, chat_id):
     con = sqlite3.connect("users.sqlite")
     cur = con.cursor()
     request = "SELECT * FROM users\
-WHERE chat_id={}".format(str(chat_id))
+\nWHERE chat_id = '{}'".format(str(chat_id))
     usrs_with_id = cur.execute(request).fetchall()
     if len(usrs_with_id):
         return False
     request = "INSERT INTO users(username, chat_id)\
-'\n'VALUES({}, {})".format(username, str(chat_id))
+\nVALUES('{}', '{}')".format(username, str(chat_id))
+    cur.execute(request).fetchall()
     return True
 
 
-def add_score():
-    pass
+def add_score(task_num, result, chat_id):
+    from datetime import datetime
+    date = datetime.now().date()
+    con = sqlite3.connect("users.sqlite")
+    cur = con.cursor()
+    request = "SELECT id FROM users\
+\nWHERE chat_id = '{}'".format(str(chat_id))
+    user_id = cur.execute(request).fetchall()
+    if not len(user_id):
+        return False
+    request = "INSERT INTO stats(user_id, task_num, date, result)\
+\nVALUES({}, {}, '{}', {})".format(user_id[0][0], task_num, date, result)
+    print(request)
+    cur.execute(request).fetchall()
+    return True
 
 
-def get_stats():
-    pass
+def get_stats(chat_id):
+    from datetime import datetime, timedelta
+    dlt = timedelta(days=7)
+    date_now = datetime.now().date()
+    con = sqlite3.connect("users.sqlite")
+    cur = con.cursor()
+    request = "SELECT * FROM stats\
+\nWHERE user_id = (SELECT id FROM users\
+\nWHERE chat_id = '{}')".format(str(chat_id))
+    result = cur.execute(request).fetchall()
+    if not len(result):
+        return False
+    nice_dick = []
+    for item in result:
+        date_list = item[3].split('-')
+        date = datetime(year=int(date_list[0]), month=int(date_list[1]), day=int(date_list[2])).date()
+        if date_now + dlt >= date:
+            nice_dick.append(item)
+        print(date)
+    print(nice_dick)
+    all_results = {}
+    for item in nice_dick:
+        if item[4] == 1:
+            if item[2] not in all_results:
+                all_results[item[2]] = 0
+            all_results[item[2]] += 1
+    print(all_results)
+    return all_results
 
+
+get_stats('лох')
