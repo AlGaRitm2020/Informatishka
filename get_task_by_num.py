ee773d44d4f639d0fbf53d8c25b9ac0ea3a1f93f
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-17 14:24:16 +0400

    get random task number before processing tasks (global optimization)

--- get_task_by_num.py
+++ get_task_by_num.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 
 
-def get_task_by_num(num):
+def get_task_by_num(task_number):
     from bs4 import BeautifulSoup
     import requests
     import random
@@ -34,116 +34,103 @@ def get_task_by_num(num):
         '26': 'cat160=on',
         '27': 'cat161=on',
     }
-    category = cat_dict[num]
-    URL = f'https://kpolyakov.spb.ru/school/ege/gen.php?action=viewAllEgeNo&egeId={num}&{category}'
+    category = cat_dict[task_number]
+    URL = f'https://kpolyakov.spb.ru/school/ege/gen.php?action=viewAllEgeNo&egeId={task_number}&{category}'
     response = requests.get(URL)
     soup = BeautifulSoup(response.text, 'html.parser')
     center = soup.find('div', class_='center')
     tasks_count = int(str(center.findAll('p')[1])[20:22])
     tasks_table = center.find('table', class_='vartopic')
+    random_task = random.randint(0, tasks_count - 1)
 
-    tasks = tasks_table.findAll('tr')[::2]
-    # print('tasks 0 text', tasks[3].find('img'), 'tasks 0 text')
-    answers = tasks_table.findAll('tr')[1::2]
-    img_addresses = []
-    excel_addresses = []
-    word_addresses = []
-    for i in range(len(tasks)):
-        tasks[i] = tasks[i].find('td', class_='topicview')
-        tasks[i] = tasks[i].find('script')
+    task = tasks_table.findAll('tr')[::2][random_task]
 
+    # print('tasks 0 text', task, 'tasks 0 text')
+    answer = tasks_table.findAll('tr')[1::2][random_task]
+    img_address = []
+    excel_address = []
+    word_address = []
+    # for i in range(len(task)):
+    task = task.find('td', class_='topicview')
+    task = task.find('script')
 
     # making img_addresses list
-    for i in range(len(tasks)):
-        txt = str(tasks[i])
-        if 'img' in txt:
-            ind = txt.find('img') + 9
-            ind1 = 0
-            for j in range(ind, len(txt)):
-                if txt[j] == '"':
-                    ind1 = j
-                    break
-            img_addresses.append(txt[ind:ind1])
-        else:
-            img_addresses.append(None)
+    txt = str(task)
+    if 'img' in txt:
+        ind = txt.find('img') + 9
+        ind1 = 0
+        for j in range(ind, len(txt)):
+            if txt[j] == '"':
+                ind1 = j
+                break
+        img_address = txt[ind:ind1]
+    else:
+        img_address = None
 
     # making answers list
-    for i in range(len(answers)):
-        answers[i] = answers[i].find('script')
-        script_txt = str(answers[i])
-        left_border_index = script_txt.find("'") + 1
-        right_border_index = script_txt.rfind("'")
-        answers[i] = [script_txt[left_border_index:right_border_index]]
-
+    answer = answer.find('script')
+    script_txt = str(answer)
+    left_border_index = script_txt.find("'") + 1
+    right_border_index = script_txt.rfind("'")
+    answer = [script_txt[left_border_index:right_border_index]]
 
     # making excel_files list
-    for i in range(len(tasks)):
-        txt = str(tasks[i])
-        if '<a' in txt and 'xls' in txt:
-            ind = txt.find('<a') + 9
-            ind1 = 0
-            for j in range(ind, len(txt)):
-                if txt[j] == '"':
-                    ind1 = j
-                    break
-            excel_addresses.append(txt[ind:ind1])
-        else:
-            excel_addresses.append(None)
+    txt = str(task)
+    if '<a' in txt and 'xls' in txt:
+        ind = txt.find('<a') + 9
+        ind1 = 0
+        for j in range(ind, len(txt)):
+            if txt[j] == '"':
+                ind1 = j
+                break
+        excel_address = txt[ind:ind1]
+    else:
+        excel_address = None
+
     # making word_files list
-    for i in range(len(tasks)):
-        txt = str(tasks[i])
-        if '<a' in txt and 'docx' in txt:
-            ind = txt.find('<a') + 9
-            ind1 = 0
-            for j in range(ind, len(txt)):
-                if txt[j] == '"':
-                    ind1 = j
-                    break
-            word_addresses.append(txt[ind:ind1])
-        else:
-            word_addresses.append(None)
-    result_tasks = []
+
+    txt = str(task)
+    if '<a' in txt and 'docx' in txt:
+        ind = txt.find('<a') + 9
+        ind1 = 0
+        for j in range(ind, len(txt)):
+            if txt[j] == '"':
+                ind1 = j
+                break
+        word_address = txt[ind:ind1]
+    else:
+        word_address = None
+
     import task_parsers
-    for i in range(len(tasks)):
-        result_tasks.append(task_parsers.get_all_tasks(tasks[i]))
 
-    for i in range(0, len(result_tasks)):
-        in_tag = False
-        html_tag = ''
-        cleaned_task = result_tasks[i]
-        # print(result_tasks[i])
-        for j in range(len(result_tasks[i])):
+    result_task = task_parsers.get_all_tasks(task)
 
-            if result_tasks[i][j] == '<' and result_tasks[i][j + 1] != ' ':
-                in_tag = True
+    in_tag = False
+    html_tag = ''
+    cleaned_task = result_task
+    # print(result_tasks[i])
+    for j in range(len(result_task)):
+        if result_task[j] == '<' and result_task[j + 1] != ' ':
+            in_tag = True
+        if in_tag:
+            html_tag += result_task[j]
+        if result_task[j] == '>' and result_task[j - 1] != ' ':
+            in_tag = False
+            if html_tag == '<br/>':
+                cleaned_task = cleaned_task.replace(html_tag, '\n')
+                html_tag = ''
+            elif html_tag == "<sup>":
+                cleaned_task = cleaned_task.replace(html_tag, '"')
+                html_tag = ''
+            else:
+                cleaned_task = cleaned_task.replace(html_tag, ' ')
+                html_tag = ''
 
-            if in_tag:
-                html_tag += result_tasks[i][j]
+        result_task = cleaned_task
 
-            if result_tasks[i][j] == '>' and result_tasks[i][j - 1] != ' ':
-                in_tag = False
-                if html_tag == '<br/>':
-                    cleaned_task = cleaned_task.replace(html_tag, '\n')
-                    html_tag = ''
-                elif html_tag == "<sup>":
+    print(result_task)
 
-                    cleaned_task = cleaned_task.replace(html_tag, '"')
-                    html_tag = ''
-
-                else:
-                    cleaned_task = cleaned_task.replace(html_tag, ' ')
-                    html_tag = ''
-
-
+    return result_task, answer, img_address, excel_address, word_address
 
 
-        result_tasks[i] = cleaned_task
-
-
-
-    for i in range(len(result_tasks)):
-      print(result_tasks[i])
-
-    num = random.randint(0, tasks_count - 1)
-    return result_tasks[num], answers[num], img_addresses[num], excel_addresses[num], word_addresses[num]
 get_task_by_num('6')
