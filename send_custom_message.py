6cee2bac6184e72949e89d2153989e6dee88696f
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-28 15:04:51 +0400

    added sending announcements to the user when the admin wants it

--- send_custom_message.py
+++ send_custom_message.py
@@ -1,8 +1,22 @@
-
 import telebot
 from config import TOKEN
+
 bot = telebot.TeleBot(TOKEN)
+chat_id = 1283628271
 
-bot.send_message(1830477841, 'Hello')
+
+announcement = "Announcement:\n" \
+                "lorem fadfasdfas\n" \
+                "dfasdfasdfa\n" \
+                "sdfasdfasdfasdf\n" \
+                "asdfasdfadsfasfd\n"
+
+@bot.message_handler(commands=['send_announcement'])
+# Объявляем функцию
+def send_announcement(message):
+    bot.send_message(chat_id,  announcement)
+    bot.send_message(message.chat.id, 'Your announcement successfully sent')
 
 
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
