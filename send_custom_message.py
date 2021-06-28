4d889c2411944f904fbd53a8b90b411e1c35d30b
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-28 16:02:24 +0400

    created an announcement for old bot address

--- send_custom_message.py
+++ send_custom_message.py
@@ -3,16 +3,21 @@ from config import TOKEN
 from sql_work import get_all_users_chat_ids
 
 bot = telebot.TeleBot(TOKEN)
+chat_ins = [i[0] for i in get_all_users_chat_ids()]
+print(chat_ins)
 chat_ids = [get_all_users_chat_ids()[3][0], get_all_users_chat_ids()[5][0]]
 print(chat_ids)
-announcement = "Test Announcement:\n" \
-               "We have a new version of the Informatishka\n"
+announcement = "*Информатишка переезжает*\n" \
+               "Мы решили изменить адрес бота на более подходящий \n" \
+               "__По этому адресу бот прекращает работу__\n" \
+               "Новый адрес: [@informatishka\_bot](https://t.me/informatishka_bot)\n " \
+
 
 
 @bot.message_handler(commands=['send_announcement'])
 def send_announcement(message):
     for chat_id in chat_ids:
-        bot.send_message(chat_id, announcement)
+        bot.send_message(chat_id, announcement, parse_mode='MarkdownV2')
     bot.send_message(message.chat.id, 'Your announcement successfully sent')
 
 
