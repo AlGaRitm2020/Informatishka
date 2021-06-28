a39fc667de94cba16da2c1346b05b7bd0ab97f28
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-28 16:40:02 +0400

    created announcement folder with markdown files

--- send_custom_message.py
+++ send_custom_message.py
@@ -1,18 +1,25 @@
 import telebot
 from config import TOKEN
 from sql_work import get_all_users_chat_ids
+TOKEN = "1764376133:AAFg2ygNpY_4I8G4WhvKplcJEKX0RCiHcKY"
+
+
+def get_announcement_from_markdown(file):
+    with open(file, mode='r', encoding='utf-8') as md:
+        announcement = md.read().replace('<br>', '')
+    return announcement
+print(get_announcement_from_markdown('data/announcements/1_migration.md'))
 
 bot = telebot.TeleBot(TOKEN)
-chat_ins = [i[0] for i in get_all_users_chat_ids()]
-print(chat_ins)
-chat_ids = [get_all_users_chat_ids()[3][0], get_all_users_chat_ids()[5][0]]
+chat_ids = [i[0] for i in get_all_users_chat_ids()]
 print(chat_ids)
+chat_ids = [1830477841]
 announcement = "*Информатишка переезжает*\n" \
                "Мы решили изменить адрес бота на более подходящий \n" \
                "__По этому адресу бот прекращает работу__\n" \
-               "Новый адрес: [@informatishka\_bot](https://t.me/informatishka_bot)\n " \
+               "Новый адрес: [@informatishka\_bot](https://t.me/informatishka_bot)\n "
 
-
+announcement = get_announcement_from_markdown('data/announcements/1_migration.md')
 
 @bot.message_handler(commands=['send_announcement'])
 def send_announcement(message):
