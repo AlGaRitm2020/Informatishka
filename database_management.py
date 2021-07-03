9cd10250567bfc2f359213fb72811f463b3b5df9
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-07-03 08:13:29 +0400

    moved db parameters to config.py, refactored database_management.py

--- database_management.py
+++ database_management.py
@@ -1,32 +1,51 @@
-DB_HOST = 'ec2-54-228-9-90.eu-west-1.compute.amazonaws.com'
-DB_NAME = 'd2gkvsrqqt4jjg'
-DB_USER = 'tkdhlpgcbebdlg'
-DB_PASS = 'cd7765e4f10eded0fecddd174a1b19e21cbb8cf6957c21289d8fd774f63efd91'
+from pprint import pprint
 
+from config import DB_HOST, DB_NAME, DB_USER, DB_PASS
 import psycopg2
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host= DB_HOST)
 
 cur = conn.cursor()
-# cur.execute('DROP TABLE stats;')
-# cur.execute('DROP TABLE users;')
-#
-# cur.execute("create table users(id serial primary key, username VARCHAR, chat_id  VARCHAR unique);")
-# cur.execute("create table stats(id serial primary key, user_id INTEGER references users, task_num INTEGER, right_answers INTEGER, all_answers INTEGER);")
-#
-# cur.execute("INSERT INTO users (username, chat_id) VALUES ('@Joyin1211', '906136828');")
-# cur.execute("INSERT INTO users (username, chat_id) VALUES ('@lelmoo', '614462421');")
-# cur.execute("INSERT INTO users (username, chat_id) VALUES ('@Aleksandr_Zhd', '1294053049');")
-# cur.execute("INSERT INTO users (username, chat_id) VALUES ('@AlGaRitm2020', '1830477841');")
-# cur.execute("INSERT INTO users (username, chat_id) VALUES ('@YulLog19', '1244102957');")
-# cur.execute("INSERT INTO users (username, chat_id) VALUES ('@albert_gareev', '1283628271');")
-# cur.execute("DELETE FROM users WHERE id = 1;")
+
+# --- drop tables ---
+""" 
+cur.execute('DROP TABLE stats;')
+cur.execute('DROP TABLE users;') 
+"""
+
+# --- create tables ---
+"""
+cur.execute("create table users(id serial primary key, username VARCHAR, chat_id  VARCHAR unique);")
+cur.execute("create table stats(id serial primary key, user_id INTEGER references users, task_num INTEGER, right_answers INTEGER, all_answers INTEGER);")
+"""
+
+# --- insert old users ---
+"""
+cur.execute("INSERT INTO users (username, chat_id) VALUES ('@Joyin1211', '906136828');")
+cur.execute("INSERT INTO users (username, chat_id) VALUES ('@lelmoo', '614462421');")
+cur.execute("INSERT INTO users (username, chat_id) VALUES ('@Aleksandr_Zhd', '1294053049');")
+cur.execute("INSERT INTO users (username, chat_id) VALUES ('@AlGaRitm2020', '1830477841');")
+cur.execute("INSERT INTO users (username, chat_id) VALUES ('@YulLog19', '1244102957');")
+cur.execute("INSERT INTO users (username, chat_id) VALUES ('@albert_gareev', '1283628271');")
+"""
+
+# --- delete data from tables ---
+"""
+cur.execute("DELETE FROM stats;")
+cur.execute("DELETE FROM users WHERE id = 4;")
+"""
+
+# --- select data from users ---
+cur.execute("SELECT * FROM users;")
+print('users', 'id, username, chat_id', sep='\n')
+pprint(cur.fetchall())
+
+# --- select data from stats ---
 cur.execute("SELECT * FROM stats;")
-# cur.execute("INSERT INTO student (name) values(%s)", ("Albert",))
-# cur.execute("DROP TABLE student;")
-print(cur.fetchall())
+print('stats', 'id, user_id, task, right_answers, all_answers', sep='\n')
+pprint(cur.fetchall())
+
+
 conn.commit()
-
 cur.close()
-
 conn.close()
--- sql_work.py
+++ sql_work.py
@@ -1,9 +1,5 @@
 import psycopg2 as psql
-
-DB_HOST = 'ec2-54-228-9-90.eu-west-1.compute.amazonaws.com'
-DB_NAME = 'd2gkvsrqqt4jjg'
-DB_USER = 'tkdhlpgcbebdlg'
-DB_PASS = 'cd7765e4f10eded0fecddd174a1b19e21cbb8cf6957c21289d8fd774f63efd91'
+from config import DB_HOST, DB_NAME, DB_USER, DB_PASS
 
 
 def register(username, chat_id):
