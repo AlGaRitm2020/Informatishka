cbcb48f8d65ca8f30b3649db3194bbae1a78fdb9
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2022-01-26 18:38:04 +0400

    added 'JoinClass' dialog

--- handlers/users/class_states.py
+++ handlers/users/class_states.py
@@ -26,22 +26,21 @@ from aiogram.types import Message, ReplyKeyboardRemove, ParseMode
 
 # --- class Menu Handlers
 @dp.message_handler(text=keyboards.default.class_captions[0], state=None)
-async def solve_tasks(message: Message):
+async def create_class_start(message: Message):
     await message.answer(f'Введите имя класса:', reply_markup=keyboards.default.back_menu)
     await states.CreateClass.enter_class_name.set()
 
 
-@dp.message_handler(text=keyboards.default.main_captions[1], state=None)
-async def theory(message: Message):
-    await message.answer(f'Вы открыли раздел с теорией \n'
-                         f'Введите номер задачи (от 1 до 27)', reply_markup=keyboards.default.back_menu)
-    await states.Theory.enter_number.set()
+@dp.message_handler(text=keyboards.default.class_captions[1], state=None)
+async def join_class_start(message: Message):
+    await message.answer(f'Введите Id класса:', reply_markup=keyboards.default.back_menu)
+    await states.JoinClass.enter_class_id.set()
 
 
 
 
 
-
+# --- Create Class Handlers
 @dp.message_handler(state=states.CreateClass.enter_class_name)
 async def enter_class_name(message: Message, state: FSMContext):
     class_name = message.text
@@ -64,24 +63,48 @@ async def enter_teacher_name(message: Message, state: FSMContext):
                          f"Id вашего класса: <b>{class_id}</b> \n"
                          f"Отправьте его ученикам, чтобы они присоединились к классу", parse_mode='html')
     #await utils.db_api.add_feedback(feedback, message.chat.id)
-    await states.CreateClass.next()
+    await states.finish()
 
 
-@dp.message_handler(Command('show_feedbacks'))
-async def show_feedbacks(message: Message, state: FSMContext):
-    if str(message.chat.id) in ADMINS:
-        
-        
-        feedbacks = await utils.db_api.get_feedbacks()
-        print(feedbacks)
-        reply_message = "Список всех отзывов\n\n"
-        for feedback, date, username in feedbacks:
-            reply_message += f"Дата:{date.day}.{date.month}.{date.year}\n" \
-                             f"Пользователь: {username}\n" \
-                             f"Отзыв: {feedback} \n\n"
-        await message.answer(reply_message)
-        #await utils.db_api.add_feedback(feedback, message.chat.id)
-        await state.finish()
-    else:
-        await message.answer("Access denied! Only for admins!")
-    await state.finish()
+
+# --- Join Class Handlers
+
+@dp.message_handler(state=states.JoinClass.enter_class_id)
+async def enter_class_id(message: Message, state: FSMContext):
+    class_id = message.text
+    class_ids = ['1', '2']
+    if class_id not in class_ids:
+        await message.answer("Класс с таким Id не существует\n"
+                             "Введите другой Id")
+        return False
+
+
+    class_name = '11A'
+    class_teacher = 'Альберт Мансурович'
+    await message.answer(f"Вы выбрали класс: {class_name}  \n"
+                         f"Учитель: {class_teacher} \n"
+                         f"Введите фамилию и имя ( так вас будет видеть учитель)")
+    #await utils.db_api.add_feedback(feedback, message.chat.id)
+    await state.update_data(class_id=class_id, class_name=class_name)
+                            
+    await states.JoinClass.next()
+
+@dp.message_handler(state=states.JoinClass.enter_student_name)
+async def enter_student_name(message: Message, state: FSMContext):
+    student_name = message.text
+    
+    students_names = ['Альберт']
+    if student_name in students_names:
+        await message.answer("Ученик с таким именем уже есть в этом классе \n"
+                             "Введите другое имя")
+        return False
+
+    data = await state.get_data()
+    class_id = data.get('class_id')
+    class_name = data.get('class_name')
+    await message.answer(f"Вы успешно присоединились к классу {class_name} как {student_name}\n")
+    #await utils.db_api.add_feedback(feedback, message.chat.id)
+    await states.finish()
+
+
+
